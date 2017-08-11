1850cd337231ec67d614bc69d7e701c77300a656
Renee Geffre <reneegeffre@wifi_stu-10-38-75-236.uncc.edu>
2017-08-11 14:31:01 -0400

    Renee's changes

---
+++ support/Garden-db.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDUj1hhm157Ukn9nn7oRO1FhdPrYOmt7PM",
+    authDomain: "garden-db.firebaseapp.com",
+    databaseURL: "https://garden-db.firebaseio.com",
+    projectId: "garden-db",
+    storageBucket: "",
+    messagingSenderId: "448077260919"
+  };
+  firebase.initializeApp(config);

2b00f74d20f4bf1584762eb065c314e15c5d91a4
Renee Geffre <reneegeffre@wifi_guest-10-38-120-163.uncc.edu>
2017-08-15 13:58:11 -0400

    search dev

--- js/rgscript.js
+++ js/rgscript.js
@@ -8,21 +8,42 @@
 var client = algoliasearch("36LOU7P2KC", "8d07c0f7efaa2dc98582aba511f6b4b7");
 var index = client.initIndex('garden_index');
 var client = algoliasearch("36LOU7P2KC", "8d07c0f7efaa2dc98582aba511f6b4b7");
-var helper = algoliasearchHelper(client, 'garden_index');
-var arr;
+var helper = algoliasearchHelper(client, 'garden_index', {
+  facets: ['type']
+});
 
 helper.on('result', function(content) {
+  //renderFacetList(content);
   renderHits(content);
   console.log(content);
 });
 
+//$('#facet-list').on('click', 'input[type=checkbox]', function(e) {
+  var facetValue = $(this).data('facet');
+  helper.toggleFacetRefinement('type', facetValue)
+        .search();
+});
+
+//function renderFacetList(content) {
+  $('#facet-list').html(function() {
+    return $.map(content.getFacetValues('type'), function(facet) {
+      var checkbox = $('<input type=checkbox>')
+        .data('facet', facet.name)
+        .attr('id', 'fl-' + facet.name);
+      if(facet.isRefined) checkbox.attr('checked', 'checked');
+      var label = $('<label>').html(facet.name + ' (' + facet.count + ')')
+                              .attr('for', 'fl-' + facet.name);
+      return $('<li>').append(checkbox).append(label);
+    });
+  });
+}
 function renderHits(content) {
   $('#container').html(function(hit) {
     return $.map(content.hits, function(hit) {
       return '<li>' + hit._highlightResult.commonName.value + '</li>';
     });
   });
-  arr = content.hits;
+
 }
 $('#search-box').on('keyup', function() {
   helper.setQuery($(this).val())

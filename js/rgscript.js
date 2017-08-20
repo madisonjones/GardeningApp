56b410d839bc26dd8464803ee8410176f738d04b
Renee Geffre <reneegeffre@RGMacBook.local>
2017-08-20 00:44:05 -0400

    final code

--- js/rgscript.js
+++ js/rgscript.js
@@ -2,7 +2,7 @@
 var client = algoliasearch('36LOU7P2KC', '4705232079a49f2115c4a0e1a5dd58fa');
 var index = client.initIndex('garden_index');
 autocomplete('#aa-search-input',
-    { hint: false }, {
+    {hint: false}, {
       source: autocomplete.sources.hits(index, {hitsPerPage: 5}),
       //value to be displayed in input control after user's suggestion selection
       displayKey: 'commonName',
@@ -13,6 +13,6 @@ autocomplete('#aa-search-input',
           return '<span>' +
               suggestion._highlightResult.commonName.value + '</span><span>' +
               suggestion._highlightResult.type.value + '</span>';
-        }
-      }
-    });
+        },
+      },
+});

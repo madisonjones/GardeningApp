82701d8d0ecfa4dbff4bc0b1c6e81d6d44b77dba
Renee Geffre <reneegeffre@RGMacBook.local>
2017-08-19 23:43:01 -0400

    final updates

--- js/rgscript.js
+++ js/rgscript.js
@@ -1,34 +1,17 @@
 // Algolia client. Mandatory to instantiate the Helper.
 var client = algoliasearch('36LOU7P2KC', '4705232079a49f2115c4a0e1a5dd58fa');
 var index = client.initIndex('garden_index');
-
-
-client.initIndex('YourIndexName').setSettings({
-  "searchableAttributes": [
-    "commonName",
-    "type",
-    "light",
-    "water"
-  ],
-  "customRanking": [
-    "desc(points)"
-  ]
-});
-
-//initialize autocomplete on search input (ID selector must match)
-autocomplete('#aa-search-input',
-{ hint: false }, {
-    source: autocomplete.sources.hits(index, {hitsPerPage: 5}),
-    //value to be displayed in input control after user's suggestion selection
-    displayKey: 'name',
-    //hash of templates used when rendering dataset
+autocomplete('#search-input', { hint: false }, [
+  {
+    source: autocomplete.sources.hits(index, { hitsPerPage: 5 }),
+    displayKey: 'my_attribute',
     templates: {
-        //'suggestion' templating function used to render a single suggestion
-        suggestion: function(suggestion) {
-          return '<span>' +
-            suggestion._highlightResult.commonName.value + '</span><span>' +
-            suggestion._highlightResult.type.value + '</span>';
-        }
+      suggestion: function(suggestion) {
+        return suggestion._highlightResult.my_attribute.value;
+      }
     }
+  }
+]).on('autocomplete:selected', function(event, suggestion, dataset) {
+  console.log(suggestion, dataset);
 });
 
